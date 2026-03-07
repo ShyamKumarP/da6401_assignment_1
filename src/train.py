3ebeeee595f1849f4b078e77e0214881db240fe2
ShyamKumarP <135097763+ShyamKumarP@users.noreply.github.com>
2026-03-07 21:39:49 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,41 +1,95 @@
-"""
-Main Training Script
-Entry point for training neural networks with command-line arguments
-"""
-
 import argparse
+import os
+import wandb
+import numpy as np
+from ann.neural_network import NeuralNetwork
+from utils.data_loader import load_data, pre_processing_data
 
 def parse_arguments():
-    """
-    Parse command-line arguments.
-    
-    TODO: Implement argparse with the following arguments:
-    - dataset: 'mnist' or 'fashion_mnist'
-    - epochs: Number of training epochs
-    - batch_size: Mini-batch size
-    - learning_rate: Learning rate for optimizer
-    - optimizer: 'sgd', 'momentum', 'nag', 'rmsprop', 'adam', 'nadam'
-    - hidden_layers: List of hidden layer sizes
-    - num_neurons: Number of neurons in hidden layers
-    - activation: Activation function ('relu', 'sigmoid', 'tanh')
-    - loss: Loss function ('cross_entropy', 'mse')
-    - weight_init: Weight initialization method
-    - wandb_project: W&B project name
-    - model_save_path: Path to save trained model (do not give absolute path, rather provide relative path)
-    """
     parser = argparse.ArgumentParser(description='Train a neural network')
-    
+
+    parser.add_argument('-d', '--dataset', type=str, default='mnist',
+                        choices=['mnist', 'fashion_mnist'])
+
+    parser.add_argument('-e', '--epochs', type=int, default=10)
+
+    parser.add_argument('-b', '--batch_size', type=int, default=64)
+
+    parser.add_argument('-l', '--loss', type=str, default='cross_entropy',
+                        choices=['mean_squared_error', 'cross_entropy'])
+
+    parser.add_argument('-o', '--optimizer', type=str, default='rmsprop',
+                        choices=['sgd', 'momentum', 'nag', 'rmsprop', 'adam', 'nadam'])
+
+    parser.add_argument('-lr', '--learning_rate', type=float, default=0.001)
+
+    parser.add_argument('-wd', '--weight_decay', type=float, default=0.0005)
+
+    parser.add_argument('-nhl', '--num_layers', type=int, default=3)
+
+    parser.add_argument('-sz', '--hidden_size', type=int, nargs='+', default=[128])
+
+    parser.add_argument('-a', '--activation', type=str, default='relu',
+                        choices=['sigmoid', 'tanh', 'relu'])
+
+    parser.add_argument('-w_i', '--weight_init', type=str, default='xavier',
+                        choices=['random', 'xavier', 'zeros'])
+
+    parser.add_argument('-w_p', '--wandb_project', type=str, default='da6401-assignment1')
+
+    parser.add_argument('--model_save_path', type=str, default='best_model.npy')
+
+    parser.add_argument('--log_gradients', action='store_true',
+                        help='Log gradient norms per layer')
+
+    parser.add_argument('--log_activations', action='store_true',
+                        help='Log dead neuron fractions')
+
     return parser.parse_args()
 
 
 def main():
-    """
-    Main training function.
-    """
     args = parse_arguments()
+    if args.loss == 'mean_squared_error':
+        args.loss = 'mse'
     
-    print("Training complete!")
+    wandb.init(project=args.wandb_project, config=vars(args))
+    X_train_raw, y_train_raw, X_test_raw, y_test_raw = load_data(args.dataset)
+    
+    X_train, y_train = pre_processing_data(X_train_raw, y_train_raw)
+    X_test, y_test = pre_processing_data(X_test_raw, y_test_raw)
 
+    if len(args.hidden_size) == 1 and args.num_layers > 1:
+        args.hidden_size = args.hidden_size * args.num_layers
+        
+    
+    model = NeuralNetwork(args=args)
+    
+    print("Starting training...")
+    model.train(
+        X_train=X_train, 
+        y_train=y_train, 
+        epochs=args.epochs, 
+        batch_size=args.batch_size, 
+        X_val=X_test, 
+        y_val=y_test
+    )
+    
+
+    save_dir = os.path.dirname(args.model_save_path)
+    if save_dir and not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    
+
+    best_weights = model.get_weights()
+    np.save(args.model_save_path, best_weights)
+
+    import json
+    with open('src/best_config.json', 'w') as f:
+        json.dump(vars(args), f, indent=4)
+        
+    wandb.finish()
+    print("Training complete!")
 
 if __name__ == '__main__':
     main()
